1541fbe4ae462068fc4a98cae7e9dd21d05e6ee4
Desumond-Osu <ssc051201@gmail.com>
2024-12-07 16:43:01 +0800

    day 6

--- day6.js
+++ day6.js
@@ -3,7 +3,7 @@ const file = fs.readFileSync(require('path').join(__dirname, 'inputs', `${requir
 
 const labMap = file.map(row => row.split(''));
 
-let [x, y] = labMap.reduce((acc, line, index) => {
+let [startX, startY] = labMap.reduce((acc, line, index) => {
   const charIndex = line.indexOf('^');
   if (charIndex !== -1) {
     acc.push(index, charIndex);
@@ -11,12 +11,14 @@ let [x, y] = labMap.reduce((acc, line, index) => {
   return acc;
 }, []);
 
+//p1
 const visitedPos = new Set();
+
+let [x, y] = [startX, startY];
 let [dirX, dirY] = [-1, 0]; //up
-const blockArr = new Set();
 
 do {
-  const key = `${x},${y} ${dirX},${dirY}`;
+  let key = `${x},${y} ${dirX},${dirY}`;
 
   if (visitedPos.has(key)) {
     break;
@@ -24,80 +26,67 @@ do {
 
   visitedPos.add(key);
 
-  let nextX = x + dirX;
-  let nextY = y + dirY;
+  let [nextX, nextY] = [x + dirX, y + dirY];
 
-  if (labMap[nextX]?.[nextY] === undefined) {
+  if (!labMap[nextX]?.[nextY]) {
     break;
   }
 
   if (labMap[nextX][nextY] === '#') {
-    temp = dirX;
-    dirX = dirY;
-    dirY = -temp;
-
-    nextX = x + dirX;
-    nextY = y + dirY;
-  } else if (![...visitedPos].some(value => value.startsWith(`${nextX},${nextY} `))) {
-    let [blockX, blockY] = [nextX, nextY];
-    let dirXInner = dirY;
-    let dirYInner = -dirX;
-
-    let nextXInner = x + dirXInner;
-    let nextYInner = y + dirYInner;
-
-    if (labMap[nextXInner]?.[nextYInner] === '#') {
-      temp = dirXInner;
-      dirXInner = dirYInner;
-      dirYInner = -temp;
-  
-      nextXInner = x + dirXInner;
-      nextYInner = y + dirYInner;
-    }
-
-    let [xInner, yInner] = [nextXInner, nextYInner];
-
-    const visitedPosInner = new Set();
-    let valid = false;
-    do {
-      const keyInner = `${xInner},${yInner} ${dirXInner},${dirYInner}`;
-
-      if (visitedPosInner.has(keyInner)) {
-        valid = true;
-        break;
-      };
-    
-      visitedPosInner.add(keyInner);
-    
-      nextXInner = xInner + dirXInner;
-      nextYInner = yInner + dirYInner;
-    
-      if (labMap[nextXInner]?.[nextYInner] === undefined) {
-        break;
-      }
-    
-      if (labMap[nextXInner][nextYInner] === '#' || (nextXInner == blockX && nextYInner == blockY)) {
-        temp = dirXInner;
-        dirXInner = dirYInner;
-        dirYInner = -temp;
-    
-        nextXInner = xInner + dirXInner;
-        nextYInner = yInner + dirYInner;
-      }
-    
-      [xInner, yInner] = [nextXInner, nextYInner];
-    } while(1);
-    
-    if (valid) {
-      if (!blockArr.has(`${blockX},${blockY}`)) {
-        blockArr.add(`${blockX},${blockY}`);
-      };
-    }
+    [dirX, dirY] = [dirY, -dirX];
+    [nextX, nextY] = [x + dirX, y + dirY];
   }
 
   [x, y] = [nextX, nextY];
 } while(1);
 
-const visitedPosArr = [new Set([...visitedPos].map(tile => tile.split(' ')[0])).size];
-console.log(visitedPosArr[0]); //p1
+const visitedPosArr = new Set([...visitedPos].map(tile => tile.split(' ')[0]));
+console.log(visitedPosArr.size);
+
+//p2
+const blockArr = new Set();
+
+visitedPosArr.forEach(block => {
+  let [blockX, blockY] = block.split(',');
+  let valid = false;
+
+  let visitedPos = new Set();
+
+  [x, y] = [startX, startY];
+  [dirX, dirY] = [-1, 0];
+
+  do {
+    let key = `${x},${y} ${dirX},${dirY}`;
+  
+    if (visitedPos.has(key)) {
+      valid = true;
+      break;
+    };
+  
+    visitedPos.add(key);
+  
+    let [nextX, nextY] = [x + dirX, y + dirY];
+  
+    if (!labMap[nextX]?.[nextY]) {
+      break;
+    }
+  
+    if (labMap[nextX][nextY] === '#' || (nextX == blockX && nextY == blockY)) {
+      [dirX, dirY] = [dirY, -dirX];
+      [nextX, nextY] = [x + dirX, y + dirY];
+
+      if (labMap[nextX][nextY] === '#' || (nextX == blockX && nextY == blockY)) {
+        [dirX, dirY] = [dirY, -dirX];
+        [nextX, nextY] = [x + dirX, y + dirY];
+      }
+    }
+  
+    [x, y] = [nextX, nextY];
+  } while(1);
+
+  if (valid) {
+    blockArr.add(`${blockX},${blockY}`);
+  }
+})
+
 console.log(blockArr.size);
