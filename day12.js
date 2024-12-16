912a0f51c7151ba56d4b2d15b35a2cd8af344c2e
Desumond-Osu <ssc051201@gmail.com>
2024-12-16 12:22:46 +0800

    day 12

--- day12.js
+++ day12.js
@@ -1,60 +1,75 @@
 const fs = require('node:fs');
 const map = fs.readFileSync(require('path').join(__dirname, 'inputs', `${require('path').basename(__filename, '.js')}.txt`), 'utf8').split(/\r?\n/).map(a => a.split(''));
 
-const directions = [
-  [1, 0],
-  [-1, 0],
-  [0, -1],
-  [0, 1],
-];
+const visitedCells = new Set();
+const directions = {
+    u: [-1, 0], r: [0, 1], l: [0, -1], d: [1, 0], ur: [-1, 1], ul: [-1, -1], dl: [1, -1], dr: [1, 1]
+};
 
-const garden = new Map();
-const history = new Set();
+const getFenceAndCorners = (map, x, y) => {
+    const [curr, u, r, l, d, ur, ul, dl, dr] = [map[x][y], ...Object.keys(directions).map(dir => {
+		const [dx, dy] = directions[dir];
+		return map[x + dx]?.[y + dy];
+	})];
 
-let gardenIndex = 0;
+    const fences = [curr != u, curr != r, curr != l, curr != d].filter(Boolean).length;
 
-map.forEach((row, rI) => {
-  row.forEach((col, cI) => {
-    const cellKey = `${rI},${cI}`;
+    const corners = [
+        !(curr == u || curr == l) || (curr == u && curr == l && curr != ul),
+		!(curr == u || curr == r) || (curr == u && curr == r && curr != ur),
+        !(curr == d || curr == l) || (curr == d && curr == l && curr != dl),
+        !(curr == d || curr == r) || (curr == d && curr == r && curr != dr)
+    ].filter(Boolean).length;
 
-    if (history.has(cellKey)) {
-      return;
-    }
+    return [fences, corners];
+};
 
-    const queue = [[rI, cI]];
-    const plantOri = map[rI][cI];
-    garden.set(gardenIndex, [0, 0]);
+const walk = (map, startX, startY) => {
+    const queue = [[startX, startY]];
+    const garden = [];
+    visitedCells.add(`${startX}-${startY}`);
 
     while (queue.length > 0) {
-      const [curX, curY] = queue.pop();
-      const cellKey = `${curX},${curY}`;
+        const [x, y] = queue.pop();
+        garden.push([x, y]);
 
-      if (history.has(cellKey)) {
-        continue;
-      }
+        for (const [dx, dy] of Object.values(directions).slice(0, 4)) { //u r l d
+            const nx = x + dx;
+            const ny = y + dy;
+            const key = `${nx}-${ny}`;
 
-      history.add(cellKey);
-      const plant = map[curX][curY];
-
-      if (plant === plantOri) {
-        const [area, peri] = garden.get(gardenIndex);
-        garden.set(gardenIndex, [area + 1, peri]);
-      }
-
-      for (const [dx, dy] of directions) {
-        const [nx, ny] = [curX + dx, curY + dy];
-
-        if (map[nx]?.[ny] === plantOri) {
-          queue.push([nx, ny]);
-        } else {
-          const [area, peri] = garden.get(gardenIndex);
-          garden.set(gardenIndex, [area, peri + 1]);
+            if (nx >= 0 && ny >= 0 && nx < map.length && ny < map[0].length && !visitedCells.has(key) && map[nx][ny] == map[x][y]) {
+                visitedCells.add(key);
+                queue.push([nx, ny]);
+            }
         }
-      }
     }
 
-    gardenIndex++;
-  });
-});
+    return garden;
+};
 
-console.log([...garden.values()].reduce((sum, [area, peri]) => sum + area * peri, 0));
+let p1 = 0;
+let p2 = 0;
+
+for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+        const key = `${i}-${j}`;
+        if (!visitedCells.has(key)) {
+            const garden = walk(map, i, j);
+            let gardenFences = 0;
+            let gardenCorners = 0; //same thing as sides
+
+            for (const [x, y] of garden) {
+                const [fences, corners] = getFenceAndCorners(map, x, y);
+                gardenFences += fences;
+                gardenCorners += corners;
+            }
+
+            p1 += garden.length * gardenFences;
+            p2 += garden.length * gardenCorners;
+        }
+    }
+}
+
+console.log(p1);
+console.log(p2);
