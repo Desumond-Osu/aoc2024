a7f1ab15b0604df097aae449000e0ae8a9e41b0b
Desumond-Osu <ssc051201@gmail.com>
2025-01-01 01:00:28 +0800

    day21

--- day21.js
+++ day21.js
@@ -1,72 +1,114 @@
 const fs = require('node:fs');
-const codes = fs.readFileSync(require('path').join(__dirname, 'inputs', `${require('path').basename(__filename, '.js')}.txt`), 'utf8').split(/\r?\n/).map(code => code.split(''));
+const codes = fs.readFileSync(require('path').join(__dirname, 'inputs', `${require('path').basename(__filename, '.js')}.txt`), 'utf8').split(/\r?\n/);
 
-const numKeypad = {
-  'A': [0, 0],
-  '0': [0, -1],
-  '1': [-1, -2],
-  '2': [-1, -1],
-  '3': [-1, 0],
-  '4': [-2, -2],
-  '5': [-2, -1],
-  '6': [-2, 0],
-  '7': [-3, -2],
-  '8': [-3, -1],
-  '9': [-3, 0],
+const number = {
+  '7': [0, 0], '8': [0, 1], '9': [0, 2],
+  '4': [1, 0], '5': [1, 1], '6': [1, 2],
+  '1': [2, 0], '2': [2, 1], '3': [2, 2],
+  '0': [3, 1], 'A': [3, 2]
 };
 
-const dirKeypad = {
-  'A': [0, 0],
-  '^': [0, -1],
-  '<': [1, -2],
-  'v': [1, -1],
-  '>': [1, 0],
+const small = {
+  '^': [0, 1], 'A': [0, 2],
+  '<': [1, 0], 'v': [1, 1], '>': [1, 2]
 };
 
-const moveString = (start, code, keypad) => {
-  let result = '';
-  code.forEach(c => {
-    let diff = keypad[c].map((value, index) => value - start[index]);
-    let logString = '';
-    //thanks to https://www.reddit.com/r/adventofcode/comments/1hjgyps/2024_day_21_part_2_i_got_greedyish/
-    if (
-      (
-        (keypad['0'] && start[1] == -2 && keypad[c][0] == 0) ||
-        (keypad['<'] && start[1] == -2) ||
-        diff[1] < 0
-      ) &&
-      (!(keypad['0'] && start[0] == 0 && keypad[c][1] == -2)) &&
-      (!(keypad['<'] && keypad[c][1] == -2))
-    ) { //leri + updo
-      logString += diff[1] > 0 ? '>'.repeat(diff[1]) : '<'.repeat(-diff[1]);
-      logString += diff[0] > 0 ? 'v'.repeat(diff[0]) : '^'.repeat(-diff[0]);
-    } else { //updo + leri
-      logString += diff[0] > 0 ? 'v'.repeat(diff[0]) : '^'.repeat(-diff[0]);
-      logString += diff[1] > 0 ? '>'.repeat(diff[1]) : '<'.repeat(-diff[1]);
+const getDir = {
+  '^': [-1, 0],
+  'v': [1, 0],
+  '<': [0, -1],
+  '>': [0, 1]
+};
+
+const perms = [];
+(function generate(arr, current) {
+  if (arr.length === 0) {
+    perms.push(current);
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      const next = arr.slice();
+      next.splice(i, 1);
+      generate(next, current.concat(arr[i]));
     }
+  }
+})([0, 1, 2, 3], []);
 
-    result += logString + 'A';
-    start = keypad[c];
-  });
+const memo = new Map();
 
-  return result;
-};
+function smallStep(py, px, ny, nx, depth, maxDepth) {
+  const key = `${py},${px},${ny},${nx},${depth}`;
 
-let totalP1 = 0;
-let totalP2 = 0;
+  if (memo.has(key)) {
+    return memo.get(key);
+  }
 
-codes.forEach(code => {
-  let string = moveString(numKeypad['A'], code, numKeypad);
+  const [dy, dx] = [ny - py, nx - px];
+  let log = Infinity;
 
-  for (let i = 0; i < 25; i++) {
-    string = moveString(dirKeypad['A'], string.split(''), dirKeypad);
-    
-    if (i == 1) {
-      totalP1 += +code.join('').match(/^\d+/)[0] * string.length;
+  for (const perm of perms) {
+    let string = '';
+    for (const p of perm) {
+      string += 
+        p == 0 && dx > 0 ? '>'.repeat(dx) :
+        p == 1 && dy > 0 ? 'v'.repeat(dy) :
+        p == 2 && dx < 0 ? '<'.repeat(-dx) :
+        p == 3 && dy < 0 ? '^'.repeat(-dy) : '';
+    }
+    string += 'A';
+
+    const [gapy, gapx] = depth >= 0 ? [0, 0] : [3, 0];
+    let [tempPy, tempPx] = [py, px];
+
+    let valid = !string.split('').some(c => {
+      if (c == 'A') {
+        return false;
+      }
+
+      const [dy, dx] = getDir[c];
+      tempPy += dy;
+      tempPx += dx;
+
+      return tempPy == gapy && tempPx == gapx;
+    });
+
+    if (valid) {
+      log = Math.min(log, walkLine(string, depth + 1, maxDepth));
     }
   }
 
-  totalP2 += +code.join('').match(/^\d+/)[0] * string.length;
-});
+  memo.set(key, log);
+  return log;
+}
 
-console.log(totalP1);
+function walkLine(line, depth, maxDepth) {
+  if (depth == maxDepth) {
+    return line.length;
+  }
+
+  const table = depth >= 0 ? small : number;
+  let [py, px] = table['A'];
+  let size = 0;
+
+  for (const c of line) {
+    const [ny, nx] = table[c];
+    size += smallStep(py, px, ny, nx, depth, maxDepth);
+    [py, px] = [ny, nx];
+  }
+
+  return size;
+}
+
+function solve(maxDepth) {
+  let total = 0;
+  memo.clear();
+
+  for (const code of codes) {
+    const n = walkLine(code, -1, maxDepth);
+    total += n * parseInt(code.match(/^\d+/)[0]);
+  }
+
+  console.log(total);
+}
+
+solve(2);
+solve(25);
